3431bd6e29d4510a506aea4efc0664bc23eb2731
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-09-27 13:28:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# config.py
+import os
 
-TOKEN = "your_bot_token"         # Replace with your actual bot token
-CHANNEL_ID = "@your_channel_id"  # Replace with your actual channel ID
+TOKEN = os.getenv("TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
