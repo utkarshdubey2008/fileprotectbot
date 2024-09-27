444a5d5d803d5b49f7be1f050d351edb93cdb5df
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-09-27 13:30:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-TOKEN = os.getenv("TOKEN")
-CHANNEL_ID = os.getenv("CHANNEL_ID")
+TOKEN = os.getenv("7376044008:AAHHp0Ap8cRGOshoPtdGZdhM_E7cBpp3uOs")
+CHANNEL_ID = os.getenv("-1002453564723")
