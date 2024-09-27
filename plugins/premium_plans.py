128feb196abe98dd6ce01e89dcfbf77ea1731d1a
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-09-27 13:22:32 +0530

    Create premium_plans.py

---
+++ plugins/premium_plans.py
@@ -0,0 +1,8 @@
+def show_plans():
+    # Logic to display premium plans
+    return """
+    Premium Plans:
+    - Basic Plan: 50 Rs
+    - Standard Plan: 100 Rs
+    - Premium Plan: 250 Rs
+    """
